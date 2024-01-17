76cf7610cc2351669395e636d0b689135f66e1e5
purple-prince <himynameischuk44@gmail.com>
2024-01-16 21:38:43 -0800

    Initial Commit

---
+++ Gem Quest/Gem_QuestApp.swift
@@ -0,0 +1,17 @@
+//
+//  Gem_QuestApp.swift
+//  Gem Quest
+//
+//  Created by Charlie Reeder on 1/16/24.
+//
+
+import SwiftUI
+
+@main
+struct Gem_QuestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
