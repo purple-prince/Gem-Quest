e47fc91a2ba0bc3a01db4d78b91ebec607d5044e
purple-prince <himynameischuk44@gmail.com>
2024-01-19 14:05:48 -0800

    resources stuff

--- Gem Quest/ContentView.swift
+++ Gem Quest/ContentView.swift
@@ -8,13 +8,54 @@
 import SwiftUI
 import Combine
 
+let tier1Ores: [String] = [
+    "Wood", "Iron"
+]
+
+// Todo: self / capturing self in swift, swiftful thinking
+
 class GameData: ObservableObject {
+    
+    let allLevels = [
+        Level(name: "Level 1", color: .red, unlockCost: 10, coinsPerSecond: 10, rawRes: ["Wood"], rawYields: [], rawRates: []),
+        Level(name: "Level 2", color: .orange, unlockCost: 100, coinsPerSecond: 100, rawRes: [], rawYields: [], rawRates: []),
+        Level(name: "Level 3", color: .yellow, unlockCost: 1_000, coinsPerSecond: 1_000, rawRes: [], rawYields: [], rawRates: []),
+        Level(name: "Level 4", color: .green, unlockCost: 10_000, coinsPerSecond: 10_000, rawRes: [], rawYields: [], rawRates: []),
+        Level(name: "Level 5", color: .teal, unlockCost: 100_000, coinsPerSecond: 100_000, rawRes: [], rawYields: [], rawRates: []),
+        Level(name: "Level 6", color: .cyan, unlockCost: 1_000_000, coinsPerSecond: 1_000_000, rawRes: [], rawYields: [], rawRates: []),
+        Level(name: "Level 7", color: .blue, unlockCost: 10_000_000, coinsPerSecond: 10_000_000, rawRes: [], rawYields: [], rawRates: []),
+        Level(name: "Level 8", color: .indigo, unlockCost: 100_000_000, coinsPerSecond: 100_000_000, rawRes: [], rawYields: [], rawRates: []),
+        Level(name: "Level 9", color: .purple, unlockCost: 1_000_000_000, coinsPerSecond: 1_000_000_000, rawRes: [], rawYields: [], rawRates: []),
+        Level(name: "Level 10", color: .brown, unlockCost: 10_000_000_000, coinsPerSecond: 10_000_000_000, rawRes: [], rawYields: [], rawRates: [])
+    ]
+    
     @Published var coins = 10
     @Published var minesUnlocked: Int = 1
+    @Published var timer: AnyCancellable?
+    @Published var activeLevels: [Level] = []
+    @Published var levelsUnlocked: Int = 0
+    
+    let updateRate: Double = 0.5
 
     func addCoins(activeLevels: [Level]) {
         for level in activeLevels {
-            coins += Int(Double(level.coinsPerSecond) * Double.random(in: 0.5..<1.5))
+            coins += Int(Double(level.coinsPerSecond) * updateRate)
+        }
+    }
+    
+    func startTimer() {
+        self.timer = Timer.publish(every: updateRate, on: .main, in: .common).autoconnect().sink { _ in
+            self.addCoins(activeLevels: self.activeLevels)
+        }
+    }
+    
+    func unlockLevel() {
+        let nextLevel = allLevels[levelsUnlocked]
+        
+        if coins >= nextLevel.unlockCost { // if we can afford the next level
+            coins -= nextLevel.unlockCost  // pay the unlock cost
+            activeLevels.append(nextLevel) // set the level to active
+            levelsUnlocked += 1            // increment the number of levels unlocked
         }
     }
 }
@@ -23,30 +64,12 @@ class GameData: ObservableObject {
 struct ContentView: View {
         
     @StateObject private var gameData = GameData()
-    @State private var timer: AnyCancellable?
-    
-    let allLevels = [
-        Level(name: "Level 1", color: .red, unlockCost: 10, coinsPerSecond: 10),
-        Level(name: "Level 2", color: .orange, unlockCost: 100, coinsPerSecond: 100),
-        Level(name: "Level 3", color: .yellow, unlockCost: 1_000, coinsPerSecond: 1_000),
-        Level(name: "Level 4", color: .green, unlockCost: 10_000, coinsPerSecond: 10_000),
-        Level(name: "Level 5", color: .teal, unlockCost: 100_000, coinsPerSecond: 100_000),
-        Level(name: "Level 6", color: .cyan, unlockCost: 1_000_000, coinsPerSecond: 1_000_000),
-        Level(name: "Level 7", color: .blue, unlockCost: 10_000_000, coinsPerSecond: 10_000_000),
-        Level(name: "Level 8", color: .indigo, unlockCost: 100_000_000, coinsPerSecond: 100_000_000),
-        Level(name: "Level 9", color: .purple, unlockCost: 1_000_000_000, coinsPerSecond: 1_000_000_000),
-        Level(name: "Level 10", color: .brown, unlockCost: 10_000_000_000, coinsPerSecond: 10_000_000_000)
-    ]
-    
-    @State var activeLevels: [Level] = []
-    @State var levelsUnlocked: Int = 0
     
     var formattedNumber: String {
         let formatter = NumberFormatter()
         formatter.numberStyle = .decimal
         return formatter.string(from: NSNumber(value: gameData.coins)) ?? ""
     }
-    
     
     var body: some View {
         VStack {
@@ -55,9 +78,7 @@ struct ContentView: View {
             ScrollView {
                 VStack {
                     
-                    
-                    
-                    ForEach(activeLevels) { level in
+                    ForEach(gameData.activeLevels) { level in
                         
                         ZStack(alignment: .center) {
                             level.color
@@ -67,30 +88,19 @@ struct ContentView: View {
                         .frame(height: 200)
                     }
                     
-                    if levelsUnlocked < allLevels.count {
-                        Text("Unlock level \(levelsUnlocked + 1) for \(allLevels[levelsUnlocked].unlockCost) coins").underline().bold()
-                            .onTapGesture {
-                                if gameData.coins >= allLevels[levelsUnlocked].unlockCost {
-                                    gameData.coins -= allLevels[levelsUnlocked].unlockCost
-                                    activeLevels.append(allLevels[levelsUnlocked])
-                                    levelsUnlocked += 1
-                                }
-                            }
+                    if gameData.levelsUnlocked < gameData.allLevels.count {
+                        Text("Unlock level \(gameData.levelsUnlocked + 1) for \(gameData.allLevels[gameData.levelsUnlocked].unlockCost) coins")
+                            .underline()
+                            .bold()
+                            .onTapGesture { gameData.unlockLevel() }
                     }
                 }
             }
-            .onAppear {
-                self.startTimer()
-            }
+            .onAppear { gameData.startTimer() }
         }
 
     }
-    
-    func startTimer() {
-        timer = Timer.publish(every: 0.5, on: .main, in: .common).autoconnect().sink { _ in
-            gameData.addCoins(activeLevels: activeLevels)
-        }
-    }
+
 }
 
 class Level: Identifiable {
@@ -101,12 +111,18 @@ class Level: Identifiable {
     let color: Color
     let unlockCost: Int
     let coinsPerSecond: Int
+    let rawRes: [String]
+    let rawYields: [Double]
+    var rawRates: [Double]
     
-    init(name: String, color: Color, unlockCost: Int, coinsPerSecond: Int) {
+    init(name: String, color: Color, unlockCost: Int, coinsPerSecond: Int, rawRes: [String], rawYields: [Double], rawRates: [Double]) {
         self.name = name
         self.color = color
         self.unlockCost = unlockCost
         self.coinsPerSecond = coinsPerSecond
+        self.rawRes = rawRes
+        self.rawYields = rawYields
+        self.rawRates = rawRates
     }
 }
 
