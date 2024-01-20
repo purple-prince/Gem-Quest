d423c76d4fd9016c64adc108ef4edd9dd858d2b2
purple-prince <himynameischuk44@gmail.com>
2024-01-20 00:19:10 -0800

    selling resources barebones functionality added, also hq sheet

---
+++ Gem Quest/HQSheetView.swift
@@ -0,0 +1,18 @@
+//
+//  HQSheetView.swift
+//  Gem Quest
+//
+//  Created by Charlie Reeder on 1/19/24.
+//
+
+import SwiftUI
+
+struct HQSheetView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HQSheetView()
+}
