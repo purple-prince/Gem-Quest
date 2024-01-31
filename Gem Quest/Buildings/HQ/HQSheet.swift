5e7124e45788e8017e108c9c12b448de974bc424
purple-prince <himynameischuk44@gmail.com>
2024-01-31 01:16:57 -0800

    added market and moved hq stuff around

---
+++ Gem Quest/Buildings/HQ/HQSheet.swift
@@ -0,0 +1,18 @@
+//
+//  HQSheet.swift
+//  Gem Quest
+//
+//  Created by Charlie Reeder on 1/31/24.
+//
+
+import SwiftUI
+
+struct HQSheet: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HQSheet()
+}
---
+++ Gem Quest/GameData.swift
@@ -0,0 +1,8 @@
+//
+//  GameData.swift
+//  Gem Quest
+//
+//  Created by Charlie Reeder on 1/31/24.
+//
+
+import Foundation
