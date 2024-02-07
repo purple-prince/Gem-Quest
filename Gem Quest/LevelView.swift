5bb95cb99cf3b7e59d7cf0df182175a1ea52c296
purple-prince <himynameischuk44@gmail.com>
2024-02-07 00:19:19 -0800

    dynamic level views

---
+++ Gem Quest/LevelView.swift
@@ -0,0 +1,18 @@
+//
+//  LevelView.swift
+//  Gem Quest
+//
+//  Created by Charlie Reeder on 2/7/24.
+//
+
+import SwiftUI
+
+struct LevelView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LevelView()
+}
